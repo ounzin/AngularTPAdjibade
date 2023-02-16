a10507eda65d72059e34527d5467c09ade6961a7
ounzin <ahmedadjibade9@gmail.com>
2023-02-16 14:22:28 +0100

    add student type

---
+++ src/mocks/students.mock.ts
@@ -0,0 +1,14 @@
+import { Student } from "../models/student";
+
+export const STUDENTS_MOCKED: Student[] = [
+  {
+    id: 1,
+    nom: "Paul",
+    prenom: "Dupont",
+  },
+  {
+    id: 2,
+    nom: "Anakin",
+    prenom: "Skywalker",
+  },
+];
